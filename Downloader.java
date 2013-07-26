f3370e49bc1492005368249d3a3dfd2a5bf19f8c
Thomas Quercia <tomquercia@gmail.com>
2013-07-26 17:16:19 -0400

    Create MessageInterface to handle incoming messages
    Also fleshed out the receiveHandshake method, and worked more on the
    Downloader.

--- Downloader.java
+++ Downloader.java
@@ -1,9 +1,12 @@
 package RUBTClient;
 
+import java.net.Socket;
+import RUBTClient.PeerInterface;
+
 public class Downloader implements Runnable {
 	public String download_ip = null;
 	public int download_port = 0;
-	
+
 	public Downloader(String ip, int port){
 		download_ip = ip;
 		download_port = port;
@@ -12,8 +15,20 @@ public class Downloader implements Runnable {
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
-		
+		Socket connection=null;
+		try{
+			Thread.sleep(30);
+		}catch (InterruptedException e){
+
+		}
+		try {
+		connection = new Socket(download_ip, download_port);
+		} catch (Exception e) {
+		}
+		PeerInterface down = new PeerInterface(connection);
+		PeerInterface.sendHandshake(RUBTClient.info_hash, RUBTClient.peer_id);
+		//PeerInterface.sendBitField();
+
 	}
 
 }
---
+++ MessageInterpreter.java
@@ -0,0 +1,5 @@
+package RUBTClient;
+
+public class MessageInterpreter {
+
+}
