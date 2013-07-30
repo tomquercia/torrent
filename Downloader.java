b6e8023a714216ce72c52040f4a9642f7b13347c
Thomas Quercia <tomquercia@gmail.com>
2013-07-30 04:26:35 -0400

    Finished the logistics behind MI and Downloader
    All that's left is the actual code implementation. That took FOREVER to
    conceptualize.

--- Downloader.java
+++ Downloader.java
@@ -1,6 +1,8 @@
 package RUBTClient;
 
 import java.net.Socket;
+
+import RUBTClient.MessageInterpreter.*;
 import RUBTClient.PeerInterface;
 
 public class Downloader implements Runnable {
@@ -10,6 +12,69 @@ public class Downloader implements Runnable {
 	public Downloader(String ip, int port){
 		download_ip = ip;
 		download_port = port;
+	}
+	
+	public void interpret(MessageInterpreter m) {
+		if (m == null) { //no message exists
+			try {
+				Thread.sleep(500L + (long) (Math.random() * 10)); 
+			} catch (Exception e) {
+				// e.printStackTrace();
+			}
+			run(); //restart connection
+			return;
+		}			
+		if (m.getId()==MessageInterpreter.bitfield) {
+			BitfieldMessage bfm = (BitfieldMessage) m;
+			//here we got a bitfield message, so set the peer's bitfield accordingly.
+			return;
+		}
+		else if (m.getId()==MessageInterpreter.keepAlive){
+			//No-op, just let timer handle it
+			return;
+		}
+		else if (m.getId()==MessageInterpreter.choke){
+			//We got a choked message, so set is as choked
+			return;
+		}
+		else if (m.getId()==MessageInterpreter.unchoke){
+			//got an unchoked message!
+			return;	
+		}		
+		else if (m.getId()==MessageInterpreter.have){
+			HaveMessage hvm = (HaveMessage) m;
+			//got a have message, update accordingly
+			return;
+		}
+		else if (m.getId()==MessageInterpreter.piece){
+			PieceMessage pm = (PieceMessage) m;
+			byte[] piece_data = pm.getData();
+			//got a piece message, now we can do the saving, 
+			//I'm not sure how to handle this, but I've included some handy functions in MessageInterpreter
+			return;
+		}
+		else if (m.getId()==MessageInterpreter.interested){
+			//Got an interested message, here we should:
+			//check the number of unchoked, and if it's less than 5, add this peer to the number of unchoked
+			//call the encode message with the peer we're sending to and the unchoke message specified
+			//else we can add it to some kind of array of pending peers
+			return;
+
+		}
+		else if (m.getId()==MessageInterpreter.notInterested){
+			//nothing to do here (I think...)
+			return;
+		}
+		else if (m.getId()==MessageInterpreter.request){
+			
+			//if the peer isn't choked, do RequestMessage whatever=(RequestMessage)m;
+			//byte[] dataToSend = new byte[whatever.getBlockLength()];
+			// then we need to generate a new piece message that we have to send out using PieceMessage
+			//then do: MessageInterpreter.encode(peer, pieceMessage)
+				return;
+		}
+
+		return;
 	}
 
 	@Override
@@ -27,8 +92,31 @@ public class Downloader implements Runnable {
 		}
 		PeerInterface down = new PeerInterface(connection);
 		PeerInterface.sendHandshake(RUBTClient.info_hash, RUBTClient.peer_id);
-		//PeerInterface.sendBitField();
-
+		if (!PeerInterface.recieveHandshake().equals(RUBTClient.info_hash)){
+			System.out.println("Handshake was unsuccessful");
+			return;
+		}
+		else{
+		//PeerInterface.sendBitField(); send the bitfield
+			//now we need their bitfield
+			MessageInterpreter d = null;
+			while(true /*peer bitfield == null*/){
+				try{
+					//d =***** need to listen for an incoming message here, maybe make receiveUnknownMessage return a MessageInterpreter object?
+				}
+				catch (Exception e){
+					run();
+					return;
+				}
+				interpret(d);
+			}
+			//now check if they are interested, listen for something from the peer, set it to 'd', then kick it out to interpret(d)
+			//Then do the whole downloading dance right here, checking for blocks, and all the bells and whistles
+			//make sure you sleep occasionally to avoid clogging up one peer for too much time.
+			
+			
+			
+		}
 	}
 
 }
--- MessageInterpreter.java
+++ MessageInterpreter.java
@@ -1,8 +1,10 @@
 package RUBTClient;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MessageInterpreter {
 	byte id;
@@ -21,47 +23,207 @@ public class MessageInterpreter {
 	public static MessageInterpreter chokeMessage = new MessageInterpreter(choke, 1);
 	public static MessageInterpreter interestedMessage = new MessageInterpreter(interested, 1);
 	public static MessageInterpreter unchokeMessage = new MessageInterpreter(unchoke, 1);
-	
+
 	protected MessageInterpreter(byte id, int length) {
 		this.id = id;
 		this.len = length;
 	}
-	
-	public MessageInterpreter handle(InputStream input) throws IOException{
-		DataInputStream data = new DataInputStream(input);
-		int messageLength = data.readInt();
-		if(messageLength == 0){
-			return keepAliveMessage;
+	public static final class InterestedMessage extends MessageInterpreter {
+		public InterestedMessage() {
+			super(interested, 1);
 		}
-		byte ident=data.readByte();
-		if(ident==choke){
-			return chokeMessage;
-		}
-		else if(ident==unchoke){
-			return unchokeMessage;
-		}
-		else if(ident==interested){
-			return interestedMessage;
-		}
-		else if(ident==notInterested){
-			return notInterestedMessage;
-		}
-		else if(ident==bitfield){
-			//bitfield message
-		}
-		else if(ident==have){
-			//have message
-		}
-		else if(ident==request){
-			//request message
-		}
-		else if(ident==piece){
-			//piecemessage
-		}
-		else{
-			System.out.println("Did not receive an ID with the message");
-		}
-		return null;
-		
 	}
+	public static final class UninterestedMessage extends MessageInterpreter {
+		public UninterestedMessage() {
+			super(notInterested, 1);
+		}
+	}
+	public static final class PieceMessage extends MessageInterpreter {
+		private final byte[] data;
+		private final int pieceIndex;
+		private final int begin;
+		public PieceMessage(final int pieceIndex, final int begin, final byte[] data) {
+			super(piece, data.length + 9);
+			this.begin = begin;
+			this.pieceIndex = pieceIndex;
+			this.data = data;
+		}
+		public int getPieceIndex() {
+			return pieceIndex;
+		}
+		public int getBegin() {
+			return begin;
+		}
+		public byte[] getData() {
+			return data;
+		}
+		public int getBlockLength() {
+			return data.length;
+		}
+	}
+	public static final class HaveMessage extends MessageInterpreter {
+
+		private final int pieceIndex;
+		public int getPieceIndex() {
+			return pieceIndex;
+		}
+		public HaveMessage(final int pieceIndex) {
+			super(have, 5);
+			this.pieceIndex = pieceIndex;
+		}
+	}
+	public static final class BitfieldMessage extends MessageInterpreter {
+		private final byte[] data;
+		public BitfieldMessage(final byte[] data) {
+			super(bitfield, 1 + data.length);
+			this.data = data;
+		}
+		public byte[] getData() {
+			return data;
+		}
+	}
+
+	public static final class RequestMessage extends MessageInterpreter {
+		private final int pieceIndex;
+		private final int begin;
+		private final int blockLength;
+
+		public RequestMessage(final int pieceIndex, final int begin, final int blockLength) {
+			super(request, 13);
+			this.pieceIndex = pieceIndex;
+			this.begin = begin;
+			this.blockLength = blockLength;
+		}
+		public int getBegin() {
+			return begin;
+		}
+		public int getPieceIndex() {
+			return pieceIndex;
+		}
+		public int getBlockLength() {
+			return blockLength;
+		}
+	}
+	public int getLength() {
+		return len;
+	}
+
+	public byte getId() {
+		return id;
+	}
+
+	public static void encode(final OutputStream out, final MessageInterpreter message)
+			throws IOException {
+		DataOutputStream sending = new DataOutputStream(out);
+		if (message.getId() == keepAlive) {
+			sending.writeInt(message.getLength());
+
+		} else {
+
+			if (message.getId()==notInterested) {
+				sending.writeInt(notInterestedMessage.getLength());
+				sending.writeByte(notInterestedMessage.getId());				
+			}
+
+			else if(message.getId()== interested){
+				sending.writeInt(interestedMessage.getLength());
+				sending.writeByte(interestedMessage.getId());
+				}
+			else if(message.getId()== choke){
+				sending.writeInt(chokeMessage.getLength());
+				sending.writeByte(chokeMessage.getId());				
+			}
+			else if(message.getId()== unchoke){
+				sending.writeInt(unchokeMessage.getLength());
+				sending.writeByte(unchokeMessage.getId());				
+			}
+			else if(message.getId()== piece){
+				PieceMessage msg = (PieceMessage) message;
+				sending.writeInt(message.getLength());
+				sending.writeByte(message.getId());
+				sending.writeInt(msg.getPieceIndex());
+				sending.writeInt(msg.getBegin());
+				sending.write(msg.getData());				
+			}			
+			else if(message.getId()== request){
+				RequestMessage req = (RequestMessage) message;
+				sending.writeInt(req.getLength());
+				sending.writeByte(req.getId());
+				sending.writeInt(req.getPieceIndex());
+				sending.writeInt(req.getBegin());
+				sending.writeInt(req.getBlockLength());
+				
+			}			
+			else if(message.getId()== bitfield){
+				BitfieldMessage bit = (BitfieldMessage) message;
+				sending.writeInt(bit.getLength());
+				sending.writeByte(bit.getId());
+				sending.write(bit.getData(), 0, bit.getData().length);
+				
+			}
+			else if(message.getId()== have){
+				HaveMessage hvm = (HaveMessage) message;
+				sending.writeInt(hvm.getLength());
+				sending.writeByte(hvm.getId());
+				sending.writeInt(hvm.getPieceIndex());
+				
+			}
+		}
+	sending.flush();
+}
+
+
+public MessageInterpreter handle(InputStream input) throws IOException{
+	DataInputStream data = new DataInputStream(input);
+	int messageLength = data.readInt();
+	if(messageLength == 0){
+		return keepAliveMessage;
+	}
+	byte ident=data.readByte();
+	if(ident==choke){
+		return chokeMessage;
+	}
+	else if(ident==unchoke){
+		return unchokeMessage;
+	}
+	else if(ident==interested){
+		return interestedMessage;
+	}
+	else if(ident==notInterested){
+		return notInterestedMessage;
+	}
+	else if(ident==bitfield){
+		byte[] dataContained;
+		int bitLength = RUBTClient.totalPieces;
+		if (bitLength % 8 == 0) {
+			dataContained = new byte[bitLength / 8];
+		} else {
+			dataContained = new byte[bitLength / 8 + 1];
+		}
+		data.readFully(dataContained);
+		return new BitfieldMessage(dataContained);		
+	}
+	else if(ident==have){
+		int index = data.readInt();
+		return new HaveMessage(index);
+	}
+	else if(ident==request){
+		int index = data.readInt();
+		int start = data.readInt();
+		int blockLength = data.readInt();
+		return new RequestMessage(index, start, blockLength);
+	}
+	else if(ident==piece){
+		int index = data.readInt();
+		int start = data.readInt();
+		byte[] dataContained = new byte[len - 9];
+		data.readFully(dataContained);
+		return new PieceMessage(index, start, dataContained);
+	}
+	else{
+		System.out.println("Did not receive an ID with the message");
+	}
+	return null;
+
+}
 }
